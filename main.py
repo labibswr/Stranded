6bb25e95ac58ea4a6f059aebbb3badc1341e8531
Labib Sarwar <147576726+labibswr@users.noreply.github.com>
2024-01-11 15:47:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,6 @@
 # Title: STRANDED PYGAME
 # Start Date: December 17, 2022
 # End Date: January 18, 2023
-# Description: The following program is a pygame project that is a game
-#               about a character lost in space. The objective of the game
-#               is for the user to complete a series of levels to get the
-#               character to the portal until it reaches the end. Upon
-#               completing the fifth level, the game is completed. If the
-#               player fails to complete a level, they are redirected to
-#               an additional "resurrection" game that is a cursor based
-#               maze game. If the user can complete the resurrection, they
-#               will be placed back at the level they were playing at.
-#               Failure of the resurrection game will result in "game over"
-#               and the user will have to replay back at level 1.
 
 # Importing pygame and classes
 from Block import *
